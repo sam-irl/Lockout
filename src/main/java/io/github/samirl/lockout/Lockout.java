536ae2acb7ba759351ad7e7761a306dc78fec0a6
sam-irl <sambirdthenerd@gmail.com>
2017-06-23 11:30:35 -0500

    more api implement + start command

--- src/main/java/io/github/samirl/lockout/Lockout.java
+++ src/main/java/io/github/samirl/lockout/Lockout.java
@@ -7,12 +7,32 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import com.mysql.jdbc.log.Log;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.flags.Flag;
+import com.sk89q.worldguard.protection.flags.StateFlag;
+import com.sk89q.worldguard.protection.flags.registry.FlagConflictException;
+import com.sk89q.worldguard.protection.flags.registry.FlagRegistry;
+
+import io.github.samirl.lockout.command.CommandLockout;
 
 public class Lockout extends JavaPlugin {
+	public static final Flag NEED_ITEM = new StateFlag("need-item", false);
 	
+	@Override
+	public void onLoad() {
+		FlagRegistry registry = getWorldGuard().getFlagRegistry();
+		try {
+			registry.register(NEED_ITEM);
+		} catch (FlagConflictException e) {
+			getLogger().severe("Fatal error: flag 'need-item' is in use by another plugin. Lockout shutting down.");
+			getLogger().severe("STACKTRACE:");
+			e.printStackTrace();
+			getLogger().severe("Fatal error: flag 'need-item' is in use by another plugin. Lockout shutting down.");
+		}
+	}
 	@Override
 	public void onEnable() {
 		Logger log = getServer().getLogger();
+		this.getCommand("lockout").setExecutor(new CommandLockout());
 	}
 	@Override
 	public void onDisable() {
---
+++ src/main/java/io/github/samirl/lockout/command/CommandLockout.java
@@ -0,0 +1,13 @@
+package io.github.samirl.lockout.command;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class CommandLockout implements CommandExecutor {
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		
+		return true;
+	}
+
+}
